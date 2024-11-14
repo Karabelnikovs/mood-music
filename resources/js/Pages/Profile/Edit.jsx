0bbba7c92a640ab917988d4286b517c05cf13a78
Karabelnikovs <125544102+Karabelnikovs@users.noreply.github.com>
2024-11-14 12:10:36 +0200

    pofile

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -8,8 +8,16 @@ export default function Edit({ mustVerifyEmail, status }) {
     return (
         <div className="bg-black h-full">
             <Head title="Profile" />
-
-            <div className="py-12">
+            <a
+                href="/"
+                className="absolute top-4 left-4 font-semibold hover:bg-black hover:text-white hover:ring hover:ring-white transition duration-300 inline-flex items-center justify-center rounded-md text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-black h-10 px-4 py-2"
+            >
+                Back
+            </a>
+            <h1 className="text-white font-extrabold text-4xl w-full flex items-center justify-center h-16">
+                Edit Profile
+            </h1>
+            <div className="pb-12">
                 <div className="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
                     <div className="bg-black p-4 shadow sm:rounded-lg sm:p-8 border-white border">
                         <UpdateProfileInformationForm
