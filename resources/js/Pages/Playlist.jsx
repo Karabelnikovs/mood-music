0aa700a90ffaba8e5de423d0ab429110a9983957
Karabelnikovs <125544102+Karabelnikovs@users.noreply.github.com>
2024-12-05 08:44:58 +0200

    fix playlists

--- resources/js/Pages/Playlist.jsx
+++ resources/js/Pages/Playlist.jsx
@@ -120,7 +120,6 @@ const Playlist = ({ playlist }) => {
                 </button>
             )}
 
-            {/* Playlist Name Modal */}
             {showModal && (
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
                     <div className="bg-gray-800 text-white p-6 rounded-lg w-full max-w-md">
